86d73139fca5d3007b5a7e77af403053f8162590
AhmadHassanAwan <ahmad.pgu1@hotmail.com>
2016-11-06 22:30:25 +0500

    This update will make sure that the script is staying updated every 5 seconds.

--- scripts/contentscript.js
+++ scripts/contentscript.js
@@ -13,20 +13,18 @@ var init = function() {
     var currentUnreadEmails = $("#" + thisParentId + " > div:first-child > span > div > div:nth-child(4) > span").text();
     if (thisSubfolderContent) {
       $("#" + thisParentId + " > div:first-child > span > div > span:nth-child(2)").trigger("click").trigger("click");
-      console.log(thisParentTitle + " contains subfolder");
       $("#" + thisParentId + " > div:nth-child(2) > div").each(function () {
         var emailCount = $(this).find("._n_p4.ms-font-m.ms-fwt-sb.ms-fcl-tp").html();
         if (emailCount) {
           unreadSubfolderEmails = unreadSubfolderEmails + parseInt(emailCount)
         }
       });
-      console.log(unreadSubfolderEmails);
       if (unreadSubfolderEmails == 0){
         return;
       }
       if (!($("#" + thisParentId + " > div:first-child > span > div > div:nth-child(4) > span").hasClass("contentAdded"))) {
         $("#" + thisParentId + " > div:first-child > span > div > div:nth-child(4) > span").addClass("contentAdded");
-        $("#" + thisParentId + " > div:first-child > span > div > div:nth-child(4) > span").html(currentUnreadEmails + " <small>(" + unreadSubfolderEmails + ")</small>");
+        $("#" + thisParentId + " > div:first-child > span > div > div:nth-child(4) > span").html(currentUnreadEmails + " <small class='unreadInSubfolders'>(" + unreadSubfolderEmails + ")</small>");
       }
       else{
         return;
@@ -43,14 +41,19 @@ var init = function() {
   });
 };
 
-/*setTimeout(function(){
-  debugger
-  $("._n_i4.treeNodeRowElement").each(function(){
-    console.log($(this));
-    $(this).trigger("click");
+var updateElements = function(){
+
+  $(".contentAdded").each(function(){
+    console.log("removing class");
+    $(this).removeClass("contentAdded");
   });
-},5000);*/
+  $("small.unreadInSubfolders").each(function(){
+    console.log("removing small");
+    $(this).remove();
+  });
+};
 
 setInterval(function(){
+  updateElements();
   init();
 },5000);
